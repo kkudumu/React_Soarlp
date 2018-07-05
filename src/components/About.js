c05c10871eeac9f45fb97947f1ddf46988fb8b5d
kkudumu <kkudumu@gmail.com>
2018-07-05 15:32:02 -0700

    About changed

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,19 @@ class About extends Component {
         <hr className="star-light mb-5"/>
         <div className="row">
           
-            <p className="lead text-center" >Mwezo Kudumu is a graduate of the East-West School for Healing Arts and has taught health and fitness programs at community, religious, and business organizations. Mr. Kudumu has over 30 years of experience in the practice of teaching and study of martial arts, physical fitness, and the healing arts. His practices include: yoga, tai chi, kung fu, judo, boxing, massage, acupressure, reflexology, meditation, and vegetarianism. He has lectured at a number of educational institutions including, San Jose State University, San Jose City College, Butte College, Chico State University, and other community organizations. Mr. Kudumu has appeared on radio, television, in the newspapers, including being the subject of major articles in the San Jose Mercury News, San Jose, California.</p>
+            <p className="lead text-center" >Mwezo Kudumu, a graduate of the East-West School for the Healing Arts has over 35 years experience
+teaching health, wellness and fitness programs to local governments, businesses and community
+organizations throughout Santa Clara County. He holds certifications to teach Evidence-based Fall
+Prevention Programs, Tai Chi, Yoga, Aerobics and Personal Training. For the past 18 years, he has been
+teaching Fall Prevention, Exercise Therapy and Older Adult Functional Fitness classes. He currently
+teaches classes at Stanford Neuroscience Center in Palo Alto, Ladera Swim and Tennis Club in Portola
+Valley, The Villages Golf and Country Club in San Jose and other community and business organizations.
+He has organized and managed Fitness and Wellness programs for the Santa Clara County Employee
+Wellness Program, YMCA, Valley Health Plan Wellness Program, American Parkinson’s Disease
+Association, Stanford Medical Center, San Jose State University, Palo Alto Parks and Recreation
+Department, San Jose City College, Butte College, Chico State University and many other business and
+community organizations. Mr. Kudumu has appeared on radio, television and, in the newspapers,
+including being the subject of major articles in the San Jose Mercury Newspaper in California.</p>
         
 
         </div>
